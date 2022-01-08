bd6d785c141bfb49917cff8a83606bafc6aabbfd
egorkuzn <egorkuzn2002@mail.ru>
2022-01-08 21:24:22 +0700

    made enough good for logic pause

--- CSV_parser/csv_parser.cpp
+++ CSV_parser/csv_parser.cpp
@@ -2,11 +2,46 @@
 
 namespace csv_parser {
 	template<class... Args>
+	CSVParser<Args...>::CSVParser(std::ifstream& in, int skipCount) {
+		std::tuple<Args...> row;
+		std::string str;
+		for (int idx = 0; idx < skipCount; ++idx)
+			getline(in, str);
+		input = &in;
+	}
+
+	template<class... Args>
 	bool CSVParser<Args...>::Iterator::operator== (const typename CSVParser<Args...>::Iterator & other) const {
 		if (input != other.input)
 			return false;
 		return position == other.position;
 	}
 
-	
+	template<class... Args>
+	bool CSVParser<Args...>::Iterator::operator!=(const typename CSVParser<Args...>::Iterator& other) const {
+		if (input != other.input)
+			return true;
+		return position != other.position;
+	}
+
+	template<class... Args>
+	typename CSVParser<Args...>::Iterator& CSVParser<Args...>::Iterator::operator++() {
+		if (input == nullptr)
+			return *this;
+		(*input) >> currentRow;
+
+		if (input->eof()) {
+			input = nullptr;
+			position = -1;
+			return *this;
+		}
+
+		++position;
+		return *this;
+	}
+
+	template<class... Args>
+	std::tuple<Args...>& CSVParser<Args...>::Iterator::operator*() {
+		return currentRow;
+	}
 }
