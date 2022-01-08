7b26e1ed975e064cb314ab6a2f68f28c7b46dec8
egorkuzn <egorkuzn2002@mail.ru>
2022-01-08 17:27:26 +0700

    making skeleton of class

--- CSV_parser/csv_parser.hpp
+++ CSV_parser/csv_parser.hpp
@@ -1,8 +1,24 @@
 #pragma once
 
+//#include <tuple>
+//#include <fstream>
+//#include <sstream>
 
 namespace csv_parser {
 	class CSVParser {
+	public:
+		class Iterator {
+		public:
 
+		private:
+		};
+	private:
+		std::ifstream* input;
+	public:
+		CSVParser(std::ifstream& in, int skipCount);
+
+		Iterator begin();
+
+		Iterator end();
 	};
 }
