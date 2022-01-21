30d5943d8970b3dcc9c9488591ee562ea94bffa0
egorkuzn <egorkuzn2002@mail.ru>
2022-01-21 20:34:21 +0700

    trying to solve problem

--- CSV_parser/my_tuple.hpp
+++ CSV_parser/my_tuple.hpp
@@ -14,21 +14,20 @@ namespace csv_parser {
 		return print_tuple(os, t, _int<Pos - 1>());
 	}
 
-	template<class A, class B, class Tuple>
-	std::basic_ofstream<A, B>& print_tuple(std::basic_ofstream<A, B>& os, const Tuple& t, _int<1>) {
-		os << std::get<std::tuple_size<Tuple>::value - 1>(t);
-		return os;
+	template<class A, class B, class Tuple, size_t Pos>
+	std::basic_ostream<A, B>& print_tuple(std::basic_ostream<A, B>& os, const Tuple& t, _int<Pos>) {
+		os << std::get<std::tuple_size<Tuple>::value - Pos>(t) << '\t';
+		return print_tuple(os, t, _int<Pos - 1>());
 	}
-
-	//template<class A, class B, class... Args>
-	//std::basic_ostream<A, B>& operator<<(std::basic_ostream<A, B>& os, const std::tuple<Args...>& t) {
-	//	print_tuple(os, t, _int<sizeof ...(Args)>());
-	//	return os;
-	//}
-
 
 	template<class A, class B, class... Args>
 	std::basic_ofstream<A, B>& operator<<(std::basic_ofstream<A, B>& os, const std::tuple<Args...>& t) {
+		print_tuple(os, t, _int<sizeof ...(Args)>());
+		return os;
+	}
+
+	template<class A, class B, class... Args>
+	std::basic_ostream<A, B>& operator<<(std::basic_ostream<A, B>& os, const std::tuple<Args...>& t) {
 		print_tuple(os, t, _int<sizeof ...(Args)>());
 		return os;
 	}
